223e19fa8a3fde270762acd8cbd04496d4fa939d
Merytek <asp3kt47@gmail.com>
2021-04-17 17:03:26 +0200

    respect des Promises

--- command/misc/ping.js
+++ command/misc/ping.js
@@ -1,9 +1,11 @@
 module.exports.run = (Conn, message, args) => {
     const m = message.channel.send("Pinging !").then(m => {
+        console.log(message => console.log(`le message c'est envoyé correctement dans : ${message.channel.name}`));
         let ping = m.createdTimestamp - message.createdTimestamp;
-
-        m.edit(`*pingpongping* Pong ! ton ping est de: **${ping}**ms`);
-    }).catch(error);
+        m.edit(`*pingpongping* Pong ! ton ping est de: **${ping}**ms`).then(me => {
+            console.log(`message édité en ${me}`);
+        }).catch(console.error);
+    }).catch(console.error);
 }
 
 module.exports.help = {
